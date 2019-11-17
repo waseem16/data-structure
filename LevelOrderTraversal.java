6fb4430ad41eaa932ea4f711e2979d16b55bdda9
Waseem <was.jmi@gmail.com>
2019-11-18 00:24:50 +0530

    added level order traversal of a binary tree

---
+++ LevelOrderTraversal.java
@@ -0,0 +1,29 @@
+public Map<Integer, List<Integer>> levelOrder(Node root) {
+		if (root == null)
+			return null;
+		Queue<Node> q = new LinkedList<Node>();
+		Map<Integer, List<Integer>> levelOrder = new HashMap<Integer, List<Integer>>();
+		List<Integer> list = new ArrayList<>();
+		int level = 0;
+		q.offer(root);
+		q.offer(null);
+		while (!q.isEmpty()) {
+			Node t = q.poll();
+			if (t != null) {
+				list.add(t.data);
+				if (t.left != null)
+					q.offer(t.left);
+				if (t.right != null)
+					q.offer(t.right);
+			} else {
+				levelOrder.put(level, list);
+				if (!q.isEmpty()) {
+					list = new ArrayList<>();
+					level++;
+					q.offer(null);
+				}
+			}
+		}
+		System.out.println(levelOrder);
+		return levelOrder;
+	}
